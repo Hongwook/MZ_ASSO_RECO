f171fb95e89206df7fab6c05ec1f0c80d2b327f4
hongwook <hwkwon@yonsei.ac.kr>
2023-03-01 21:10:12 +0900

    Initial Commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import warnings
+from datetime import datetime, timedelta
+
+from database import *
+
+warnings.filterwarnings("ignore")
+
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import association_rules, fpgrowth
+
+### data import
+end_date = datetime.now().strftime('%Y-%m-%d')
+start_date = (datetime.now() - timedelta(days=365 * 2)).strftime('%Y-%m-%d')  # 최근 2년간의 판매 데이터를 바탕으로 연관분석 진행
+query_obj = Queries(start_date, end_date)
+db_obj = DBImport(db_type='cscart')
+
+analytics = db_obj.data_import(query_obj.analytics_query)
+# 재고 테이블 로드 및 전처리
+inventory = db_obj.data_import(query_obj.inventory_query)
+inventory = inventory[inventory['sys_time'] == inventory['sys_time'].max()]
+prod_bar = analytics.sort_values(by='purchased_at', ascending=False)[['product_id', 'barcode']].drop_duplicates(
+    subset='barcode')  # 가장 최근 기준으로 상품코드-바코드 매칭
+inventory = pd.merge(inventory, prod_bar, on='barcode', how='left').groupby('product_id')['amount'].sum().reset_index()
+inventory = inventory.rename(columns={'product_id': 'consequents'})
+
+### association analysis
+prod_order = analytics[['order_id', 'product_id']].drop_duplicates()
+item_list = prod_order.groupby('order_id')['product_id'].apply(list).tolist()
+
+te = TransactionEncoder()
+te_result = pd.DataFrame(te.fit_transform(item_list), columns=te.columns_)
+te_result.head()
+
+frequent_itemsets = fpgrowth(te_result, use_colnames=True, min_support=0.0001)
+result = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+
+# postprocessing
+prod_name = analytics.sort_values(by='purchased_at', ascending=False)[
+    ['product_id', 'product_name_kor']].drop_duplicates(subset='product_id', keep='first')
+# change frozenset to value and filtering multi itemsets
+result['antecedents'] = result['antecedents'].apply(lambda x: list(x)[0] if len(list(x)) == 1 else 'multi')
+result['consequents'] = result['consequents'].apply(lambda x: list(x)[0] if len(list(x)) == 1 else 'multi')
+result = result[(result['antecedents'] != 'multi') & (result['consequents'] != 'multi')]
+result = pd.merge(result, prod_name, left_on='antecedents', right_on='product_id', how='left')
+result = result.rename(columns={'product_name_kor': 'ante_prod_nm'})
+result = pd.merge(result, prod_name, left_on='consequents', right_on='product_id', how='left')
+result = result.rename(columns={'product_name_kor': 'cons_prod_nm'})
+result = result.drop(['product_id_x', 'product_id_y'], axis=1)
+result = pd.merge(result, inventory, on='consequents', how='left')
+result['amount'] = result['amount'].fillna(0)
+result = result[['antecedents', 'consequents', 'lift', 'amount', 'ante_prod_nm', 'cons_prod_nm']].sort_values(
+    by=['antecedents', 'lift'], ascending=[True, False]).head(10)
